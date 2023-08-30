27a96c17bf36c51f5742a72f3564f13992acec00
kdan80 <kierandansey80@gmail.com>
2023-08-30 12:18:56 +0100

    added siteMetadata

---
+++ config/siteMetadata.ts
@@ -0,0 +1,15 @@
+const siteMetadata = {
+    title: 'Kieran Dansey',
+    keywords: [
+        'Kieran Dansey',
+        'Kieran',
+        'Dansey',
+        'Freelance Web Developer',
+        'Web Developer',
+    ] as const,
+    description: 'Kieran Dansey freelance web developer',
+    siteUrl: 'https://kierandansey.co.uk',
+    image: '/images/og.webp',
+} as const
+
+export default siteMetadata
