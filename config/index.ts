dc4eb9ccd310398119865ec679fe5931c619e051
kdan80 <kierandansey80@gmail.com>
2023-08-30 13:05:30 +0100

    config is now a module

---
+++ config/index.ts
@@ -0,0 +1,40 @@
+import siteMetadata from './siteMetadata'
+import openGraph from './openGraph'
+//import { Github, LinkedIn, Codepen } from 'components/icons'
+
+const details = {
+    email: 'kierandansey80@gmail.com',
+
+    skills: [
+        'HTML',
+        'CSS/SASS',
+        'Tailwind',
+        'Javascript (ES6+)',
+        'React/Next.js',
+        'Typescript',
+        'Node.js/Express',
+        'Python/Django',
+        'php/Laravel',
+        'Linux/Bash',
+        'MongoDB',
+        'Git',
+        'SVG/Inkscape',
+    ],
+
+    // socialLinks: [
+    //     {
+    //         url: 'https://github.com/kdan80',
+    //         icon: Github,
+    //     },
+    //     {
+    //         url: 'https://linkedin.com/in/kdansey80',
+    //         icon: LinkedIn,
+    //     },
+    //     {
+    //         url: 'https://codepen.io/kdan80',
+    //         icon: Codepen,
+    //     },
+    // ],
+}
+
+export { details, siteMetadata, openGraph }
