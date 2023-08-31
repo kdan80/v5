f37a69bbac73c13e0f3912fb33137606b492a028
kdan80 <kierandansey80@gmail.com>
2023-08-31 19:23:54 +0100

    remove redundant import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { siteMetadata, openGraph, details, siteConfig } from '../../config'
-import BackgroundImage from '@/components/BackgroundImage'
 
 const inter = Inter({ subsets: ['latin'] })
 
