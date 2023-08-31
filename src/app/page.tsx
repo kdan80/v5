05407e347fa05fb88d50548a93cee67e52ed9e96
kdan80 <kierandansey80@gmail.com>
2023-08-31 19:35:48 +0100

    Added Header.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Header } from '@/components'
 
 const Fragment = React.Fragment
 
 const Home = () => {
     return (
         <Fragment>
+            <Header />
             <div className='h-screen w-screen border'>Hello 1</div>
             <div className='h-screen w-screen border'>Hello 2</div>
             <div className='h-screen w-screen border'>Hello 3</div>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='w-full h-12 bg-red-500 flex items-center'>Header</header>
+}
+
+export default Header
