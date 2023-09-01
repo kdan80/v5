7bc6731d4577bb7f08fd2adbdbc05ade25c5876b
kdan80 <kierandansey80@gmail.com>
2023-09-01 14:54:35 +0100

    fixed height in px

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavLink from './NavLinks'
 import { siteConfig } from '@/config'
+import BigButton from '../BigButton'
 
 const Header = () => {
     return (
-        <header className='fixed top-0 w-full p-[5vw] md:px-12 md:py-10 flex justify-between items-center'>
+        <header className='fixed top-0 w-full h-[100px] px-12 flex justify-between items-center'>
             <a
                 href='#landing'
                 className='text-white text-sm hover:text-green font-sans'
@@ -12,15 +13,18 @@ const Header = () => {
                 <span className='text-green '>&lt;/&gt;&nbsp;&nbsp;</span>
                 <span className='transition-colors ease-in-out duration-500'>kieran dansey</span>
             </a>
-            <nav className='hidden md:flex gap-8'>
-                {siteConfig.navLinks.map((link, index) => (
-                    <NavLink
-                        index={index + 1}
-                        url={link.url}
-                        name={link.name}
-                    />
-                ))}
-            </nav>
+            <div className='hidden md:flex items-center'>
+                <nav className='flex gap-8 mr-8'>
+                    {siteConfig.navLinks.map((link, index) => (
+                        <NavLink
+                            index={index + 1}
+                            url={link.url}
+                            name={link.name}
+                        />
+                    ))}
+                </nav>
+                <BigButton className='px-4 py-2'>Resume</BigButton>
+            </div>
         </header>
     )
 }
