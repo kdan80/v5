b420db4009c7d7ec4f6d73c47c94e9db07e3907f
kdan80 <kierandansey80@gmail.com>
2023-09-06 19:24:02 +0100

    various components added

---
+++ src/components/Header/DownloadCV.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import BigButton from '../BigButton'
+
+const DownloadCV = () => {
+    return <BigButton className='px-4 py-2'>Download CV</BigButton>
+}
+
+export default DownloadCV
