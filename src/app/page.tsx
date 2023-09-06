4640c2098700e29fc33f3199e6a78c6444c0e10e
kdan80 <kierandansey80@gmail.com>
2023-09-06 21:24:43 +0100

    page dimensions added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { Header } from '@/components'
 
 const Home = () => {
     return (
-        <main className='text-light-200'>
+        <main className='text-light-200 min-h-screen max-w-screen overflow-x-hidden'>
             <Header />
-            <div className='h-screen w-screen flex items-center justify-center'>
+            <div className='h-screen w-full flex items-center justify-center'>
                 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
             </div>
-            <div className='h-screen w-screen flex items-center justify-center'>Hello 2</div>
-            <div className='h-screen w-screen flex items-center justify-center'>Hello 3</div>
+            <div className='h-screen w-full flex items-center justify-center'>Hello 2</div>
+            <div className='h-screen w-full flex items-center justify-center'>Hello 3</div>
         </main>
     )
 }
