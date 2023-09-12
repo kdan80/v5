245dca9379a771f03783ee1b154de2ee6c9f5920
kdan80 <kierandansey80@gmail.com>
2023-09-12 14:40:15 +0100

    get rid of conditional logic and add base styles

--- src/components/Widgets/BaseWidget.tsx
+++ src/components/Widgets/BaseWidget.tsx
@@ -1,27 +1,21 @@
 import React from 'react'
-import { HTMLTag, BorderColor } from './widget.types'
+import { HTMLTag } from './widget.types'
 
 interface Props {
     className?: string
     children?: React.ReactNode
-    borderColor?: BorderColor
     debug?: true
     as?: HTMLTag
 }
 
-const BaseWidget = ({ className, children, borderColor, as, debug }: Props) => {
+const BaseWidget = ({ className, children, as, debug }: Props) => {
     const Component = as || 'div'
 
-    //prettier-ignore
-    // This is required to prevent class='undefined' polluting the final html
-    const classes =
-        (debug ? 'border-2 border-[#ff0000]' + ' ' : '') +
-        (className ? className : '')
+    const debugClass = debug ? 'border-2 border-[#ff0000]' : ''
 
-    // This is required to prevent class='' polluting the final html
-    if (!classes) return <Component>{children}</Component>
-
-    return <Component className={classes}>{children}</Component>
+    return (
+        <Component className={`text-light-200  ${debugClass} ${className}`}>{children}</Component>
+    )
 }
 
 export default BaseWidget
