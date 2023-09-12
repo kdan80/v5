f746e00f19d2efbc803b81f775e5ac26dda4c9db
kdan80 <kierandansey80@gmail.com>
2023-09-12 14:31:32 +0100

    change borderColor to a debug flag

--- src/components/Widgets/BaseWidget.tsx
+++ src/components/Widgets/BaseWidget.tsx
@@ -5,39 +5,18 @@ interface Props {
     className?: string
     children?: React.ReactNode
     borderColor?: BorderColor
+    debug?: true
     as?: HTMLTag
 }
 
-//prettier-ignore
-export interface IBorderColors {
-    red: 'border-[#ff0000]'
-    green: 'border-[#00ff00]'
-    blue: 'border-[#0000ff]'
-    yellow: 'border-[#ffff00]'
-    white: 'border-[#ffffff'
-    black: 'border-[#000000]'
-    purple: 'border-[#ff00ff]'
-}
-
-//prettier-ignore
-const borderColors: IBorderColors = {
-    red: 'border-[#ff0000]',
-    green: 'border-[#00ff00]',
-    blue: 'border-[#0000ff]',
-    yellow: 'border-[#ffff00]',
-    white: 'border-[#ffffff',
-    black: 'border-[#000000]',
-    purple: 'border-[#ff00ff]',
-} as const
-
-const BaseWidget = ({ className, children, borderColor, as }: Props) => {
+const BaseWidget = ({ className, children, borderColor, as, debug }: Props) => {
     const Component = as || 'div'
 
     //prettier-ignore
     // This is required to prevent class='undefined' polluting the final html
     const classes =
-        (className ? className : '') +
-        (borderColor ? ' ' + `border-2 ${borderColors[borderColor as keyof IBorderColors]}` : '')
+        (debug ? 'border-2 border-[#ff0000]' + ' ' : '') +
+        (className ? className : '')
 
     // This is required to prevent class='' polluting the final html
     if (!classes) return <Component>{children}</Component>
