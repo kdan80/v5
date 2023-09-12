25e7b2f6c724d89dae9672213d1dfa023eae9c86
kdan80 <kierandansey80@gmail.com>
2023-09-12 13:34:05 +0100

    add basic padding and styles

--- src/components/Widgets/Container.tsx
+++ src/components/Widgets/Container.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import BaseWidget from './BaseWidget'
 
-import RootContainer from './RootContainer'
+interface Props {
+    children?: React.ReactNode
+}
 
-const Container = () => {
-    return <RootContainer className=''>hello world</RootContainer>
+const Container = ({ children }: Props) => {
+    return (
+        <BaseWidget
+            className='px-6 md:px-12 flex items-center'
+            as='div'
+        >
+            {children}
+        </BaseWidget>
+    )
 }
 
 export default Container
