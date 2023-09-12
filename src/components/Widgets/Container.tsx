06d470e739605bbd4489559bc870ed601b3149a3
kdan80 <kierandansey80@gmail.com>
2023-09-12 14:32:08 +0100

    move borderColor to a debug flag

--- src/components/Widgets/Container.tsx
+++ src/components/Widgets/Container.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import BaseWidget from './BaseWidget'
+import { HTMLTag, BorderColor } from './widget.types'
 
 interface Props {
     children?: React.ReactNode
+    className?: string
+    as?: HTMLTag
+    debug?: true
 }
 
-const Container = ({ children }: Props) => {
+const Container = ({ children, className, as, debug }: Props) => {
     return (
         <BaseWidget
-            className='px-6 md:px-12 flex items-center'
-            as='div'
+            className={`px-6 md:px-12 flex items-center ${className}`}
+            as={as}
+            debug={debug}
         >
             {children}
         </BaseWidget>
