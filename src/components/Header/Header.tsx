5b8de9682dec39afed3540604aac401af050641e
kdan80 <kierandansey80@gmail.com>
2023-09-13 12:00:49 +0100

    move subcomponents to their own files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,25 @@
 import React from 'react'
-import NavLink from './NavLinks'
-import { siteConfig } from '@/config'
 import DownloadCV from './DownloadCV'
 import { DropdownMenu } from '..'
 import HeaderBackdrop from './HeaderBackdrop'
-//import { Container as HeaderContainer } from '@/Widgets'
 import HeaderContainer from './components/HeaderContainer'
+import HomeLink from './components/HomeLink'
+import NavLinks from './components/NavLinks'
+import FlexContainer from './components/FlexContainer'
 
 const Header = () => {
     return (
         <HeaderContainer>
             {/* Client component */}
             <HeaderBackdrop />
-            <a
-                href='#landing'
-                className='text-white text-lg md:text-sm hover:text-green font-sans'
-            >
-                <span className='text-green '>&lt;/&gt;&nbsp;&nbsp;</span>
-                <span className='transition-colors ease-in-out duration-500'>kieran dansey</span>
-            </a>
-            <div className='hidden md:flex items-center'>
-                <nav className='flex gap-8 mr-8'>
-                    {siteConfig.navLinks.map((link, index) => (
-                        <NavLink
-                            key={index}
-                            index={index + 1}
-                            url={link.url}
-                            name={link.name}
-                        />
-                    ))}
-                </nav>
+
+            <HomeLink>kieran dansey</HomeLink>
+
+            <FlexContainer>
+                <NavLinks />
                 <DownloadCV />
-            </div>
+            </FlexContainer>
+
             {/* Client component */}
             <DropdownMenu />
         </HeaderContainer>
