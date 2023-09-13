9d4c7216e85cf5f343cf75dbc40fdede1174386c
kdan80 <kierandansey80@gmail.com>
2023-09-13 12:01:01 +0100

    creat subcomponent files

---
+++ src/components/Header/components/NavLinks.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { siteConfig } from '@/config'
+
+const NavLinks = () => {
+    return (
+        <nav className='flex gap-8 mr-8'>
+            {siteConfig.navLinks.map((link, index) => (
+                <a
+                    className='text-sm text-light-300 md:text-light-200 hover:text-green font-mono transition-colors ease-in-out duration-500'
+                    href={link.url}
+                >
+                    <span className='text-green text-xs align-baseline'>0{index}.&nbsp;</span>
+                    <span className='align-baseline'>{link.name}</span>
+                </a>
+            ))}
+        </nav>
+    )
+}
+
+export default NavLinks
