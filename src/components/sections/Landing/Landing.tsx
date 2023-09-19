ec87b5fe148546379bf321c10bbc3663a04d91d0
kdan80 <kierandansey80@gmail.com>
2023-09-19 21:27:10 +0100

    orchestrate animations using web animation api + useref

--- src/components/sections/Landing/Landing.tsx
+++ src/components/sections/Landing/Landing.tsx
@@ -2,37 +2,72 @@
 
 import React from 'react'
 import { Section } from '@/sharedComponents'
-import Link from 'next/link'
 import { BigButton } from '@/sharedComponents'
 
 const Landing = () => {
-    const [pageLoaded, setPageLoaded] = React.useState(false)
+    const refs = React.useRef<HTMLElement[]>([])
 
-    React.useEffect(() => {
-        setPageLoaded(true)
+    const pushToRefsArray = (el: HTMLElement | null) => {
+        el && refs.current.push(el)
+    }
+
+    React.useLayoutEffect(() => {
+        const fadeUpElements = refs.current
+
+        fadeUpElements.forEach((element, index) => {
+            element.animate(
+                [
+                    {
+                        transform: 'translateY(20px)',
+                        opacity: 0,
+                    },
+                    {
+                        transform: 'translateY(0)',
+                        opacity: 1,
+                    },
+                ],
+                {
+                    duration: 250,
+                    fill: 'forwards',
+                    easing: 'cubic-bezier(0.42, 0, 0.58, 1)',
+                    delay: index * 75,
+                }
+            )
+        })
     }, [])
 
     return (
         <Section id='landing'>
             <h1
-                className={`text-green font-mono text-landingH1 mb-2 transition ease-in-out duration-300 opacity-0 translate-y-[20px] ${
-                    pageLoaded ? 'translate-y-[0] opacity-100' : ''
-                }`}
+                ref={el => pushToRefsArray(el)}
+                className='opacity-0 text-green font-mono text-landingH1 mb-2'
             >
                 Hi, my name is
             </h1>
-            <h2 className='font-semibold leading-[.9] text-landingH2 text-light-100'>
+            <h2
+                ref={el => pushToRefsArray(el)}
+                className='opacity-0 font-semibold leading-[.9] text-landingH2 text-light-100'
+            >
                 Kieran Dansey.
             </h2>
-            <h3 className='font-semibold leading-[.9] text-landingH2 text-light-900'>
+            <h3
+                ref={el => pushToRefsArray(el)}
+                className='opacity-0 font-semibold leading-[.9] text-landingH2 text-light-900'
+            >
                 I build things for the web.
             </h3>
-            <p className='md:max-w-[60%] text-light-900 leading-[1.5rem] text-lg'>
+            <p
+                ref={el => pushToRefsArray(el)}
+                className='opacity-0 md:max-w-[60%] text-light-900 leading-[1.5rem] text-lg'
+            >
                 I am a freelance web developer, and occasional designer, with a passion for building
                 exceptional digital experiences.
             </p>
 
-            <div className='mt-8'>
+            <div
+                ref={el => pushToRefsArray(el)}
+                className='opacity-0  mt-8'
+            >
                 <BigButton className='px-6 py-4'>
                     <a href='#projects'>View My Projects</a>
                 </BigButton>
