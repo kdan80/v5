4031ce9181489b134444ab66c898489220aef86b
kdan80 <kierandansey80@gmail.com>
2023-09-20 00:15:47 +0100

    use navLink.id instead of index

--- src/components/header/components/NavLinks.tsx
+++ src/components/header/components/NavLinks.tsx
@@ -4,12 +4,12 @@ import { siteConfig } from '@/config'
 const NavLinks = () => {
     return (
         <nav className='flex gap-8 mr-8'>
-            {siteConfig.navLinks.map((navLink, index) => (
+            {siteConfig.navLinks.map(navLink => (
                 <a
                     className='text-sm text-light-300 md:text-light-200 hover:text-green font-mono transition-colors ease-in-out duration-500'
                     href={navLink.url}
                 >
-                    <span className='text-green text-xs align-baseline'>0{index}.&nbsp;</span>
+                    <span className='text-green text-xs align-baseline'>0{navLink.id}.&nbsp;</span>
                     <span className='align-baseline'>{navLink.name}</span>
                 </a>
             ))}
