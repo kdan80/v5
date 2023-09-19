30ecb2fe716c55baf0ada161cdfdd0aa941bfaa7
kdan80 <kierandansey80@gmail.com>
2023-09-20 00:30:38 +0100

    move repeated tw styles into module

---
+++ src/styles/index.ts
@@ -0,0 +1,3 @@
+const bigButton = `text-green border border-green rounded text-sm font-mono transition-all duration-300 ease-in-out hover:bg-heroHover focus:bg-heroHover active:bg-heroHover`
+
+export { bigButton }
