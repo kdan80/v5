b0a9f0855f40743328cd7e7033635693fbee2a10
kdan80 <kierandansey80@gmail.com>
2023-09-20 20:19:05 +0100

    added About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Header } from '@/components'
-import { Landing } from '@/sections'
+import { About, Landing } from '@/sections'
 
 const Home = () => {
     return (
         <main className='text-light-200 min-h-screen max-w-screen overflow-x-hidden'>
             <Header />
             <Landing />
+            <About />
             <div className='h-screen w-full flex items-center justify-center'>Hello 2</div>
             <div className='h-screen w-full flex items-center justify-center'>Hello 3</div>
         </main>
