ec7cce9e30a164124c10321f9acf6385165e90ff
kdan80 <kierandansey80@gmail.com>
2023-09-20 11:49:07 +0100

    move components into header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,27 @@
+'use client'
+
 import React from 'react'
-import { BigButton } from '@/sharedComponents'
 import { bigButton } from '@/styles'
 import { DropdownClientContext } from '..'
-import HeaderBackdrop from './components/HeaderBackdrop'
-import HeaderContainer from './components/HeaderContainer'
-import HomeLink from './components/HomeLink'
 import { siteConfig } from '@/config'
+import useScrolledToTop from '../../useScrolledToTop'
 
 const Header = () => {
-    return (
-        <HeaderContainer>
-            {/* Client component */}
-            <HeaderBackdrop />
+    const scrolledToTop = useScrolledToTop()
 
-            <HomeLink>kieran dansey</HomeLink>
+    return (
+        <header
+            className={`fixed w-full h-[80px] md:h-[100px] px-6 md:px-12 flex items-center justify-between transition ease-in-out duration-300 ${
+                scrolledToTop ? '' : 'backdrop-blur-sm shadow-x md:bg-[#15171888]'
+            }`}
+        >
+            <a
+                href='#landing'
+                className='text-white text-lg md:text-sm hover:text-green font-sans'
+            >
+                <span className='text-green '>&lt;/&gt;&nbsp;&nbsp;</span>
+                <span className='transition-colors ease-in-out duration-500'>kieran dansey</span>
+            </a>
 
             <div className='hidden md:flex items-center'>
                 <nav className='flex gap-8 mr-8'>
@@ -39,7 +47,7 @@ const Header = () => {
 
             {/* Client component */}
             <DropdownClientContext />
-        </HeaderContainer>
+        </header>
     )
 }
 
