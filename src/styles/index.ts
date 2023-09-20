57cdec0bdc3d481731b8990990cba934ea02ce04
kdan80 <kierandansey80@gmail.com>
2023-09-20 15:19:50 +0100

    add section and sectionContent

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,7 @@
-const bigButton = `text-green border border-green rounded text-sm font-mono transition-all duration-300 ease-in-out hover:bg-heroHover focus:bg-heroHover active:bg-heroHover`
+const bigButton = `text-green cursor-pointer border border-green rounded text-sm font-mono transition-all duration-300 ease-in-out hover:bg-heroHover focus:bg-heroHover active:bg-heroHover`
 
-export { bigButton }
+const section = `flex justify-center items-center min-h-screen px-6 md:px-12 w-screen border`
+
+const sectionContent = `flex flex-col justify-between gap-6 font-sans border`
+
+export { bigButton, section, sectionContent }
