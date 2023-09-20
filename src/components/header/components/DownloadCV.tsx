82279271d1d1c40db7298352b6dcfcff88e7be3e
kdan80 <kierandansey80@gmail.com>
2023-09-20 11:49:23 +0100

    delete components and move into header

--- src/components/header/components/DownloadCV.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { BigButton } from '@/sharedComponents'
-
-const DownloadCV = () => {
-    return <BigButton className='px-4 py-2'>Download CV</BigButton>
-}
-
-export default DownloadCV
