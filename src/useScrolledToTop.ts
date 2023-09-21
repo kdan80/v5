eb6ff04462f3cb0a626196dc33f311ebc7cfeb26
kdan80 <kierandansey80@gmail.com>
2023-09-21 01:23:33 +0100

    use intersection observer instead on scroll event handler

--- src/useScrolledToTop.ts
+++ src/useScrolledToTop.ts
@@ -1,22 +1,57 @@
 import React from 'react'
 
-const useScrolledToTop = (): boolean => {
+const THRESHOLD = 500
+
+const useScrolledToTop = (ref: any): boolean => {
     const [scrolledToTop, setScrolledToTop] = React.useState(true)
 
     React.useEffect(() => {
-        const handleScroll = () => {
-            if (window.innerWidth > 768) return setScrolledToTop(window.scrollY < 100)
-            return setScrolledToTop(window.scrollY === 0)
+        const obs = ref.current
+        if (!obs) return
+
+        const cb = (entries: any) => {
+            const [entry] = entries
+
+            setScrolledToTop(entry.isIntersecting)
         }
 
-        window.addEventListener('scroll', handleScroll)
+        const observer = new IntersectionObserver(cb, {
+            rootMargin: '100px 0px 0px 0px',
+            threshold: 1,
+        })
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+        observer.observe(obs)
+
+        return () => observer.unobserve(obs)
+    }, [ref])
 
     return scrolledToTop
 }
 
 export default useScrolledToTop
+
+// import React from 'react'
+
+// const THRESHOLD = 10
+
+// const useScrolledToTop = (): boolean => {
+//     const [scrolledToTop, setScrolledToTop] = React.useState(true)
+//     const lastScrollY = React.useRef(0)
+
+//     React.useEffect(() => {
+//         const handleScroll = () => {
+//             if (window.innerWidth > 768) return setScrolledToTop(window.scrollY < 100)
+//             return setScrolledToTop(window.scrollY === 0)
+//         }
+
+//         window.addEventListener('scroll', handleScroll)
+
+//         return () => {
+//             window.removeEventListener('scroll', handleScroll)
+//         }
+//     }, [])
+
+//     return scrolledToTop
+// }
+
+// export default useScrolledToTop
