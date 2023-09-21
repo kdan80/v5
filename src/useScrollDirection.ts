00a13581abcae21fc4703f7bd7eb87dd703c8850
kdan80 <kierandansey80@gmail.com>
2023-09-21 11:13:02 +0100

    remove comments

--- src/useScrollDirection.ts
+++ src/useScrollDirection.ts
@@ -4,6 +4,7 @@ import React from 'react'
 // It uses the window scrollY positon and compares it with the previous value.
 // If the difference between the values is positive we are scrolling
 // down, if negative we are scrolling up.
+// It will only update if the users scrolls more than the THRESHOLD value in pixels
 
 type ScrollDirection = 'up' | 'down'
 const THRESHOLD = 200
@@ -44,47 +45,3 @@ const useScrollDirection = (): ScrollDirection => {
 }
 
 export default useScrollDirection
-
-// import React from 'react'
-
-// // This hook keeps track of scroll direction to be used in animations elsewhere.
-// // It uses the window scrollY positon and compares it with the previous value.
-// // If the difference between the values is positive we are scrolling
-// // down, if negative we are scrolling up.
-
-// type ScrollDirection = 'up' | 'down'
-
-// const useScrollDirection = (): ScrollDirection => {
-//     const THRESHOLD = 200
-//     const [scrollDirection, setScrollDirection] = React.useState<ScrollDirection>('up')
-
-//     React.useEffect(() => {
-//         let lastScrollY = window.scrollY
-//         let ticking = false
-
-//         const updateScrollDirection = () => {
-//             const currentScrollY = window.scrollY
-
-//             if (Math.abs(scrollY - lastScrollY) < THRESHOLD) return (ticking = false)
-
-//             setScrollDirection(currentScrollY > lastScrollY ? 'down' : 'up')
-//             lastScrollY = currentScrollY > 0 ? currentScrollY : 0
-//             return (ticking = false)
-//         }
-
-//         const onScroll = () => {
-//             if (!ticking) {
-//                 window.requestAnimationFrame(updateScrollDirection)
-//                 ticking = true
-//             }
-//         }
-
-//         window.addEventListener('scroll', onScroll)
-
-//         return () => window.removeEventListener('scroll', onScroll)
-//     }, [])
-
-//     return scrollDirection
-// }
-
-// export default useScrollDirection
